cdd01aa8694b76df79c20b6d72d78830a827df34
habeebullahokunlola@gmail.com <habeebullahokunlola@gmail.com>
2023-10-21 02:10:22 +0100

    Gave my nav-bar a certain function if i scroll to some certain amount

--- main.js
+++ main.js
@@ -4,3 +4,16 @@ const navBtn = document.querySelector('.open-nav')
 navBtn.addEventListener('click', () => {
    body.classList.toggle('open')
 })
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY >= 200) {
+       body.classList.add("nav-design")
+        // Your scroll position is greater than or equal to 200
+        // You can now apply the function to your nav-bar
+        // For example, you can add a CSS class or modify its style
+    } else {
+       body.classList.remove("nav-design")
+        // Your scroll position is less than 200
+        // You can reset the nav-bar to its initial state
+    }
+});
